4b8410d7fbb2f139d9f1440a2c364e4f3689f9ab
Benjamin Stuermer <benjamin.stuermer@pnnl.gov>
2017-11-06 13:48:27 -0800

    Added error handling for the event that a type returns an empty set of records

--- application/src/PacificaSearchBundle/Controller/SearchController.php
+++ application/src/PacificaSearchBundle/Controller/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace PacificaSearchBundle\Controller;
 
+use PacificaSearchBundle\Model\ElasticSearchTypeCollection;
 use PacificaSearchBundle\Repository\InstrumentRepository;
 use PacificaSearchBundle\Repository\InstrumentTypeRepository;
 use PacificaSearchBundle\Repository\InstitutionRepository;
@@ -14,10 +15,20 @@ class SearchController extends Controller
 {
     public function indexAction()
     {
+        /** @var ElasticSearchTypeCollection[] */
         $filters = array_map(function ($repoClass) {
             /** @var Repository $repo */
             $repo = $this->container->get($repoClass);
             $instances = $repo->getAll()->sortByDisplayName();
+
+            // If a repo returns an empty set then something has gone wrong
+            if (!count($instances)) {
+                throw new \RuntimeException(
+                    "No records found in $repoClass::getAll(), this is probably an error in your Elastic Search "
+                  . "configuration or the corresponding type in your Elastic Search database is not populated"
+                );
+            }
+
             return $instances;
         }, [
             InstrumentTypeRepository::class,
