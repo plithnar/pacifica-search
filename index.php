6bdc5402d9d37561f965a73c29336b41403d8845
David Brown <dmlb2000@gmail.com>
2017-11-28 17:42:21 -0800

    this should actually run the linting (#8)
    * this should actually run the linting
    
    * phpcs just the things we have for php
    
    * forgot the tests directory
    
    * add some docs to make phpcs happy
    
    * class document vs module document
    
    * let's try fixing more stuff
    
    * we need return statement but we don't return anything

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * Index.php for example application
+ *
+ * @category Index.php
+ * @package  Main
+ * @author   David Brown <dmlb2000@gmail.com>
+ * @license  https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html GNU Lesser General Public License, version 2.1
+ * @link     https://github.com/pacifica/pacifica-search
+ */
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,6 +21,7 @@
             <li>
                 <a href="/show.php?id=<?= $x ?>">
                     <?= $x ?>
+                    <? $x += 1; ?>
                 </a>
             </li>
             <?php endwhile ?>
--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -1,14 +1,48 @@
 <?php
+/**
+ * Selenium PHP Driver Module
+ *
+ * @category Module
+ * @package  Tests
+ * @author   David Brown <dmlb2000@gmail.com>
+ * @license  https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html GNU Lesser General Public License, version 2.1
+ * @link     https://github.com/pacifica/pacifica-search
+ */
+
+/**
+ * Selenium PHP Driver Index.php tests
+ *
+ * @category Class
+ * @package  MyClass
+ * @author   David Brown <dmlb2000@gmail.com>
+ * @license  https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html GNU Lesser General Public License, version 2.1
+ * @link     https://github.com/pacifica/pacifica-search
+ */
 class WebTest extends PHPUnit_Extensions_Selenium2TestCase
 {
     protected $coverageScriptUrl = 'http://localhost:8193/phpunit_coverage.php';
 
+    /**
+     * Setup the selenium test environment with firefox
+     *
+     * This function sets up the selenium browser with firefox and sets the url to
+     * the correct localhost address for our testing environment.
+     *
+     * @return Nothing
+     */
     protected function setUp()
     {
         $this->setBrowser('firefox');
         $this->setBrowserUrl('http://localhost:8192/');
     }
 
+    /**
+     * Test the title of the main page to see if it's correct.
+     *
+     * This function tests the main page and checks for the title so we know it's correct.
+     *
+     * @return Nothing
+     */
     public function testTitle()
     {
         $this->url('http://localhost:8192/');
