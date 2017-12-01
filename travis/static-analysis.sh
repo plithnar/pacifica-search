b75595c2f5f19665e61a6409d9876e41c06408c6
Benjamin Stuermer <benjamin.stuermer@pnnl.gov>
2017-12-01 11:48:22 -0800

    Updated static analysis's syntax check to exclude the non-existent index.php and to include the source files of our Symfony bundle

--- travis/static-analysis.sh
+++ travis/static-analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
 # basic syntax check against all php files
-find tests index.php -name '*.php' | xargs -n 1 php -l
+find tests application/src -name '*.php' | xargs -n 1 php -l
 ./vendor/bin/phpcs -n --extensions=php --ignore=*/vendor/* --standard=pacifica_php_ruleset.xml tests index.php
