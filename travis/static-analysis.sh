08d861b1ee568d4e3aa6682a77697898523ec821
Benjamin Stuermer <benjamin.stuermer@pnnl.gov>
2017-12-01 12:06:44 -0800

    Replaced index.php with Pacifica bundle path in PHP Code Sniffer static analysis

--- travis/static-analysis.sh
+++ travis/static-analysis.sh
@@ -2,4 +2,4 @@
 
 # basic syntax check against all php files
 find tests application/src -name '*.php' | xargs -n 1 php -l
-./vendor/bin/phpcs -n --extensions=php --ignore=*/vendor/* --standard=pacifica_php_ruleset.xml tests index.php
+./vendor/bin/phpcs -n --extensions=php --ignore=*/vendor/* --standard=pacifica_php_ruleset.xml tests application/src/*
