2e6e3ef98b633f16ef85044d6ddf380bc2624563
Benjamin Stuermer <benjamin.stuermer@pnnl.gov>
2017-12-01 11:51:40 -0800

    Removed non-existent index.php from unit-tests.sh output

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -xe
 
 if ! ./vendor/bin/phpunit --verbose --coverage-text tests ; then
-  ls index.php*
   cat /tmp/selenium-server.log || true
   cat travis/error.log || true
   cat travis/access.log || true
