7c3919aa715e63280278fe5366a1174270593c59
Ken Auberry <kauberry@mac.com>
2018-01-03 13:19:03 -0800

    Adding in a bunch of common third-party scripts, css, images, icons, etc.

--- application/src/PacificaSearchBundle/Controller/SearchController.php
+++ application/src/PacificaSearchBundle/Controller/SearchController.php
@@ -12,6 +12,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SearchController extends Controller
 {
+    private $page_data = [];
+    public function __construct()
+    {
+        $this->page_data['script_uris'] = array(
+            'js/lib/spinner/spin.min.js',
+            'js/lib/fancytree/dist/jquery.fancytree-all.js',
+            'js/lib/select2/dist/js/select2.js'
+        );
+        $this->page_data['css_uris'] = array(
+            'js/lib/fancytree/dist/skin-lion/ui.fancytree.min.css',
+            'js/lib/select2/dist/css/select2.css',
+            'css/file_directory_styling.css',
+            'css/combined.css'
+        );
+    }
     public function indexAction()
     {
         /** @var ElasticSearchTypeCollection[] */
@@ -38,7 +53,8 @@ class SearchController extends Controller
         return $this->render(
             'PacificaSearchBundle::search.html.twig',
             [
-                'filters' => $filters
+                'filters' => $filters,
+                'page_data' => $this->page_data
             ]
         );
     }
