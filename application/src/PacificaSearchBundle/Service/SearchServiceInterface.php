19530daffc811f192d9dcbc49a19395f1cd8b682
Benjamin Stuermer <benjamin.stuermer@pnnl.gov>
2018-06-04 14:27:23 -0700

    Removed the $assertResultsFound parameter from getResults() because we weren't using it anywhere any more

--- application/src/PacificaSearchBundle/Service/SearchServiceInterface.php
+++ application/src/PacificaSearchBundle/Service/SearchServiceInterface.php
@@ -2,9 +2,6 @@
 
 namespace PacificaSearchBundle\Service;
 
-use Elasticsearch\Client;
-use Elasticsearch\ClientBuilder;
-
 interface SearchServiceInterface
 {
     /**
@@ -16,11 +13,9 @@ interface SearchServiceInterface
 
     /**
      * @param ElasticSearchQueryBuilder $queryBuilder
-     * @param bool $assertResultsFound Pass TRUE to throw an exception if results aren't found - useful for early
-     *   detection of a misconfigured or misconnected database for queries that we know should always return results.
      * @return array The results of the search
      */
-    public function getResults(ElasticSearchQueryBuilder $queryBuilder, $assertResultsFound = false);
+    public function getResults(ElasticSearchQueryBuilder $queryBuilder);
 
     /**
      * Retrieve only the IDs of the fields matched by a query
