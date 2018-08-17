d17764aca534c5b6a61e81f1d39a977f5a403101
Degan, Michael G <michael.degan@pnnl.gov>
2018-08-17 13:15:57 -0700

    Moving files from /web/css and /web/js to /web/assets/css and /web/assets/js in an effort to make the deployed site work better/easier.

--- application/src/PacificaSearchBundle/Controller/GuiController.php
+++ application/src/PacificaSearchBundle/Controller/GuiController.php
@@ -32,15 +32,15 @@ class GuiController
 
     private $page_data = [
         'script_uris' => [
-            'js/lib/spinner/spin.min.js',
-            'js/lib/fancytree/dist/jquery.fancytree-all.js',
-            'js/lib/select2/dist/js/select2.js'
+            'assets/js/lib/spinner/spin.min.js',
+            'assets/js/lib/fancytree/dist/jquery.fancytree-all.js',
+            'assets/js/lib/select2/dist/js/select2.js'
         ],
         'css_uris' => [
-            'js/lib/fancytree/dist/skin-lion/ui.fancytree.min.css',
-            'js/lib/select2/dist/css/select2.css',
-            'css/file_directory_styling.css',
-            'css/combined.css'
+            'assets/js/lib/fancytree/dist/skin-lion/ui.fancytree.min.css',
+            'assets/js/lib/select2/dist/css/select2.css',
+            'assets/css/file_directory_styling.css',
+            'assets/css/combined.css'
         ]
     ];
 
